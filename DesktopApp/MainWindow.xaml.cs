744d9ae0882b711a2249ec700463e05f51a5919a
Ilian Shchepinskyi <iliantheone@gmail.com>
2025-05-08 17:08:50 +0300

    Moved changes from master to developer

---
+++ DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DesktopApp;
+using System.Windows;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        this.Content = new Pages.SignInPage();
+    }
+}
